84bf9c9448cbeb86c9f4c25900d281f4e7cc5765
EduYube <eduyuberol@gmail.com>
2025-03-19 11:41:20 +0100

    creacion conexion a la bdd

---
+++ enpoints/connection.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'admin'
+});
+
+connection.connect((err) => {
+    if(err){
+        console.log('Error al conectar a la base de datos', err);
+        return;
+    }
+    connection.query('CREATE DATABASE IF NOT EXISTS lvdl', (err) => {
+        if(err){
+            console.log('Error al crear la base de datos', err);
+            return;
+        }
+    });
+    connection.changeUser({database: 'lvdl'}, (err) => {
+        if(err){
+            console.log('Error al seleccionar la base de datos', err);
+            return;
+        }
+    });
+    connection.query('CREATE TABLE IF NOT EXISTS provincias (id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(50))', (err) => {
+        if(err){
+            console.log('Error al crear la tabla provncias', err);
+            return;
+        }
+    });
+    console.log('Conectado a la base de datos');
+});
